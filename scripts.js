e695598c6621d579a30287313bc9a69fe5fc6663
Irina Akimenkova <68510263+IrinaAkimenkova@users.noreply.github.com>
2020-12-08 16:11:02 +0500

    Добавила удаление текста на страницу входа.

--- scripts.js
+++ scripts.js
@@ -4,6 +4,21 @@
 // style.href = chrome.extension.getURL('logo.png');
 // document.getElementsByTagName('head')[0].appendChild(style);
 
+/****СТРАНИЦА ВХОДА****/
+/*Удаление текста и перенос его в другой элемент*/
+/*Скрипт выполняется только на двух страницах*/
+if (window.location.href === 'https://student.psu.ru/pls/stu_cus_et/stu.timetable' || 'https://student.psu.ru/pls/stu_cus_et/stu.login') {
+	/*Удаление текста при входе в ЕТИС*/
+	const div2 = document.getElementsByClassName("items")[0];
+	const index2 = div2.innerHTML.indexOf("По всем вопросам звоните по телефону 2396870. Специалист службы технической поддержки находится в кабинете 245 (бывший кабинет отдела пропусков), расположенном между первым и вторым корпусом на 2 этаже.");
+	if (index2 >= 0) {
+		const etis_text = div2.innerHTML.substring(index2);
+		div2.innerHTML = div2.innerHTML.substring(0, index2);
+		const headerMessage = document.getElementsByClassName('header_message')[0];
+		headerMessage.innerText = etis_text;
+	}
+}
+
 /****ПРОПУЩЕННЫЕ ЗАНЯТИЯ****/
 
 /*Смена цвета текста "Всего пропущено занятий: */
